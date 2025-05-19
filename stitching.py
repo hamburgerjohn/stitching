63b249204391273e33616218ce8e7380d565c95e
hamburgerjohn <joebob2154@gmail.com>
2025-05-19 13:12:37 -0400

    Added dot recordings

--- stitching.py
+++ stitching.py
@@ -1,22 +1,28 @@
 from PyQt5.QtWidgets import (
     QApplication, QMainWindow, QComboBox, QOpenGLWidget, 
-    QWidget, QVBoxLayout, QHBoxLayout, QPushButton
+    QWidget, QVBoxLayout, QHBoxLayout, QPushButton,
+    QDockWidget, QTextEdit
 )
 from PyQt5.QtCore import Qt
 import numpy as np 
 from OpenGL.GL import *
-from OpenGL.GLUT import glutInit, glutBitmapCharacter
+from OpenGL.GLUT import glutInit, glutBitmapCharacter, GLUT_BITMAP_9_BY_15
 from OpenGL.GLU import *
 import sys
+from utility import read_txt
+import ctypes
+
+GLUT_BITMAP_HELVETICA_10 = ctypes.c_void_p(5)
+
 
 class GLWidget(QOpenGLWidget):
 
     def __init__(self, parent=None):
         super().__init__(parent)
 
-        self.points = np.array([
-            [0.0,0.0],
-            [0.0,0.5],
+        self.points = [
+            [0.000,0.000],
+            [0.000,0.500],
             [0.0,-0.5],
             [0.5,0.0],
             [0.5,0.5],
@@ -24,16 +30,21 @@ class GLWidget(QOpenGLWidget):
             [-0.5,0.0],
             [-0.5,0.5],
             [-0.5,-0.5],
-        ], dtype=np.float32)
+        ]
 
-        self.camera_x = 0.0
-        self.camera_y = 0.0
-        self.camera_step = 0.1
+        self.camera_x = 1000.0
+        self.camera_y = 1000.0
+        self.camera_step_x = 50.0
+        self.camera_step_y = 40.0
         self.view_size = 1.0
-        self.zoom = 1.0
-        self.zoom_step = 0.1
+        self.zoom = 1000.0
+        self.zoom_step = 50.0
         self.show_labels = False
         self.show_lines = False
+        self.record_point = False
+        self.recorded_points = []
+        self.prediction_curves = []
+        self.recording_dock_text = QTextEdit()
 
         #Allows openGl widget to be focused
         #without it, only focuses on GUI 
@@ -51,7 +62,7 @@ class GLWidget(QOpenGLWidget):
 
     def paintGL(self):
         
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+        glClear(int(GL_COLOR_BUFFER_BIT) | int(GL_DEPTH_BUFFER_BIT))
 
         glLoadIdentity()
         
@@ -66,22 +77,20 @@ class GLWidget(QOpenGLWidget):
         glLoadIdentity()
 
         glPointSize(5.0)
-        glColor3f(1.0, 1.0, 1.0)
+        glColor3f(1.0, 1.0, 0.0)
 
         #draw point array
         glEnableClientState(GL_VERTEX_ARRAY)
         glVertexPointer(2, GL_FLOAT, 0, self.points)
         glDrawArrays(GL_POINTS, 0, len(self.points))
         glDisableClientState(GL_VERTEX_ARRAY)
-
         
-
         # draw labels
         if self.show_labels:
             glColor3f(1.0, 1.0, 1.0)
             for x, y in self.points:
-                label = f"({x:.1f},{y:.1f})"
-                self.drawText2D(x - 0.05, y - 0.07, label)
+                label = f"({x:.3f},{y:.3f})"
+                self.drawText2D(x - 0.01, y - 0.01, label)
 
 
         #save matrices of movable objects         
@@ -96,6 +105,7 @@ class GLWidget(QOpenGLWidget):
         #switch to screen space to draw fixed horizontal line 
         ###################################################################
         glOrtho(0, self.width(), 0, self.height(), -1, 1)
+
 
         #draw lines 
         if not self.show_lines:
@@ -115,8 +125,27 @@ class GLWidget(QOpenGLWidget):
 
         glMatrixMode(GL_MODELVIEW)
         glPopMatrix()
-       
 
+        #record singular point
+        if self.record_point:
+
+            left = self.camera_x - variable_size
+            right = self.camera_x + variable_size
+            bottom = self.camera_y - variable_size
+            top = self.camera_y + variable_size
+
+            visible_points = [
+                (x,y) for x,y in self.points
+                if left <= x <= right and bottom <= y <= top
+            ]
+
+            if len(visible_points) == 1:
+                print(visible_points)
+                self.recorded_points.append(visible_points[0])
+                self.recorded_points = list(set(self.recorded_points))
+                self.recorded_points.sort(key=lambda x: x[0])
+                self.recording_dock_text.clear()
+                self.recording_dock_text.append("".join([str(x) + "\n" for x in self.recorded_points]))
 
         glFlush()
        
@@ -125,7 +154,7 @@ class GLWidget(QOpenGLWidget):
         glRasterPos(x,y)
 
         for ch in string:
-            glutBitmapCharacter(GLUT_BITMAP_HELVETICA_10, ord(ch))
+            glutBitmapCharacter(GLUT_BITMAP_9_BY_15, ord(ch))
 
 
     def keyPressEvent(self, event):
@@ -133,13 +162,13 @@ class GLWidget(QOpenGLWidget):
         key = event.key()
 
         if key == Qt.Key.Key_Left:
-            self.camera_x += self.camera_step
+            self.camera_x += self.camera_step_x
         elif key == Qt.Key.Key_Right:
-            self.camera_x -= self.camera_step
+            self.camera_x -= self.camera_step_x
         elif key == Qt.Key.Key_Up:
-            self.camera_y -= self.camera_step
+            self.camera_y -= self.camera_step_y
         elif key == Qt.Key.Key_Down:
-            self.camera_y += self.camera_step
+            self.camera_y += self.camera_step_y
         elif key == Qt.Key.Key_Plus:
             self.zoom = max(0.1, self.zoom - self.zoom_step)
         elif key == Qt.Key.Key_Minus:
@@ -147,6 +176,28 @@ class GLWidget(QOpenGLWidget):
         
 
         self.update()
+
+class RecordingDock(QDockWidget):
+
+    def __init__(self, widget):
+        super().__init__()
+
+        layout = QVBoxLayout()
+        layout.addWidget(widget)
+
+        self.container = QWidget()
+        self.container.setLayout(layout)
+        self.container.setContentsMargins(0,60,0,0)
+
+        self.setWidget(self.container)
+        self.setAllowedAreas(Qt.RightDockWidgetArea)
+        self.setTitleBarWidget(QWidget())
+        self.hide()
+
+    def addWidget(self, widget):
+        self.widget = widget
+
+    
 
 
 
@@ -159,30 +210,44 @@ class MainWindow(QMainWindow):
         #Create dropdown list 
         self.combo = QComboBox(self)
         self.combo.addItems(["Camera Course", "Camera Fine", "Camera Super Fine"])
-        self.combo.currentIndexChanged.connect(self.cameraSpeedSelect)
+        self.combo.currentIndexChanged.connect(self.onCameraSpeedSelect)
 
         
         #Create toggleable button and attaches event handler
-        self.coordsToggle = QPushButton(self)
-        self.coordsToggle.setCheckable(True)
-        self.coordsToggle.setText("Toggle Coordinates")
-        self.coordsToggle.toggled.connect(self.onToggleLabels)
+        self.coordsToggle = self.create_toggle_button("Toggle Coordinates", self.onToggleLabels, True)
 
-        self.linesToggle = QPushButton(self)
-        self.linesToggle.setCheckable(True)
-        self.linesToggle.setText("Toggle Lines")
-        self.linesToggle.toggled.connect(self.onToggleLines)
+        self.linesToggle = self.create_toggle_button("Toggle Lines", self.onToggleLines, True)
+
+        self.recordToggle = self.create_toggle_button("Toggle Record", self.onToggleRecord, True)
+
+        self.resetRecord = QPushButton(self)
+        self.resetRecord.setText("Confirm Record")
+        self.resetRecord.clicked.connect(self.onResetToggle)
+
+        self.openRecordings = QPushButton(self)
+        self.openRecordings.setText("Open Recordings")
+        self.openRecordings.clicked.connect(self.onOpenRecordings)
+
 
         #add openGL widget
         self.glWidget = GLWidget(self)
 
         #horizontal layout
         hLayout1 = QHBoxLayout()
-        hLayout1.addWidget(self.combo)
-        hLayout1.addWidget(self.coordsToggle)
+        hLayout1 = self.add_widgets_to_layout(
+            hLayout1,
+            self.combo,
+            self.coordsToggle,
+            self.linesToggle, 
+        )
 
         hLayout2 = QHBoxLayout()
-        hLayout2.addWidget(self.linesToggle)
+        hLayout2 = self.add_widgets_to_layout(
+            hLayout2,
+            self.recordToggle,
+            self.resetRecord,
+            self.openRecordings
+        )
 
         #main layout is vertical
         mainLayout = QVBoxLayout()
@@ -190,23 +255,51 @@ class MainWindow(QMainWindow):
         mainLayout.addLayout(hLayout2)
         mainLayout.addWidget(self.glWidget)
 
-        container = QWidget()
-        container.setLayout(mainLayout)
-        self.setCentralWidget(container)
+        
+        self.recording_dock = RecordingDock(self.glWidget.recording_dock_text)
+        self.addDockWidget(Qt.RightDockWidgetArea,self.recording_dock)
+        self.resizeDocks([self.recording_dock], [int(self.width()* 0.25)], Qt.Horizontal)
+
+        
+
+        main_container = QWidget()
+        main_container.setLayout(mainLayout)
+        self.setCentralWidget(main_container)
+
+    #helper func
+    def create_toggle_button(self, title, eventHandler, checkable):
+        toggleButton = QPushButton(self)
+        toggleButton.setCheckable(checkable)
+        toggleButton.setText(title)
+        toggleButton.toggled.connect(eventHandler)
+
+        return toggleButton
+
+    
+    
+    def add_widgets_to_layout(self, layout, *args):
+        for i , args in enumerate(args):
+            layout.addWidget(args)
+
+        return layout
+
 
     #event handlers
-    def cameraSpeedSelect(self, index):
+    def onCameraSpeedSelect(self, index):
 
         if index == 0:
-            self.glWidget.camera_step = 0.1
+            self.glWidget.camera_step_x = 0.1
+            self.glWidget.camera_step_y= 0.1
             self.glWidget.zoom_step = 0.1
 
         elif index == 1:
-            self.glWidget.camera_step = 0.01
+            self.glWidget.camera_step_x = 0.01
+            self.glWidget.camera_step_y= 0.01
             self.glWidget.zoom_step = 0.01
         
         elif index == 2:
-            self.glWidget.camera_step = 0.001
+            self.glWidget.camera_step_x = 0.001
+            self.glWidget.camera_step_y= 0.001
             self.glWidget.zoom_step = 0.01
             
         
@@ -220,11 +313,47 @@ class MainWindow(QMainWindow):
         self.glWidget.show_lines = checked
         self.glWidget.update()
 
+    def onToggleRecord(self, checked):
+        self.recording_dock.show()
+        self.glWidget.record_point = checked
+        self.glWidget.update()
+
+    def onResetToggle(self, checked):
+
+        if not self.glWidget.record_point:
+
+            self.recording_dock.hide()
+
+            if len(self.glWidget.recorded_points) > 0:
+                self.glWidget.prediction_curves.append(set(self.glWidget.recorded_points))
+
+            self.glWidget.recorded_points.clear()
+            self.glWidget.recording_dock_text.clear()
+            print(self.glWidget.prediction_curves)
+            self.glWidget.update()
+    
+    def onOpenRecordings(self, checked):
+        pass
+    #utility func
+    def set_coords(self, file1, file2):
+        
+        x_points = read_txt(file1)
+        y_points = read_txt(file2)
+        coords = []
+
+        for i, x in enumerate(x_points):
+            coords.append([x, y_points[i]])
+
+        self.glWidget.points = coords
+
+        self.glWidget.update()
+
 if __name__ == "__main__":
     
     glutInit()
     app = QApplication(sys.argv)
     window = MainWindow()
-    window.resize(600,600)
+    window.set_coords("GetX.TXT", "GetY.TXT")
+    window.resize(1000,1000)
     window.show()
     sys.exit(app.exec_())
---
+++ utility.py
@@ -0,0 +1,95 @@
+import re, os, sys, datetime
+import numpy as np
+
+def error(e):
+
+    if e is None:
+        print("-------------------------------------------------")
+        print(f"Failed to Open File")
+        print("-------------------------------------------------")
+
+    else:
+        print("-------------------------------------------------")
+        print(f"Command failed with return code {e}")
+        print("-------------------------------------------------")
+
+
+def sortby_creation_date(files : list):
+
+    files = sorted(files, key = lambda x: datetime.datetime.fromtimestamp(os.path.getctime(x)), reverse=True)
+
+    files_dates = {}
+
+    for file in files:
+        files_dates.setdefault(file, datetime.datetime.fromtimestamp(os.path.getctime(file)).strftime("%B %d, %Y - %H:%M:%S"))
+    
+    return files_dates
+
+
+def get_file(file : str, drive : str):
+    
+    files_ = []
+
+    for root, dir, files in os.walk(drive + ":\\"):
+        for f in files:
+            if f == file:
+                files_.append(os.path.join(root, f))
+
+    files_ = sortby_creation_date(files_)
+
+    return files_
+
+def read_txt(get_file : str):
+    
+    f = open(get_file, "r", encoding="UTF-8")
+
+    data = []
+
+    for line in f.readlines():
+
+
+        line = [x for x in re.split(r"\s{2,}", line) if x != '' and x != '\n']
+
+        if len(line) > 6:
+            
+            if "Measured" not in line:
+                data.append(line[1])
+
+    f.close()
+
+    return [float(x) for x in data]
+
+def get_current_drive():
+
+    try:
+        exe_dir = os.path.dirname(os.path.abspath(sys.argv[0])) 
+
+    except:
+        print("Error getting drive of this exe")
+
+    return exe_dir[0] 
+
+
+x_points = read_txt("GetX.TXT")
+y_points = read_txt("GetY.TXT")
+coords = []
+
+for i, x in enumerate(x_points):
+    coords.append([x, y_points[i]])
+
+print(coords)
+
+
+
+
+
+
+
+
+    
+
+    
+
+
+
+
